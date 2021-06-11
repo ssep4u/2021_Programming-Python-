01cacdea3aa5ceb1b4b57bb961ca0bc1117b53ec
ssep4u <ssep4u@gmail.com>
2021-06-11 16:20:13 +0900

    room1, 2

--- 2400/수행평가/floor.py
+++ 2400/수행평가/floor.py
@@ -1,10 +1,10 @@
 import room
-
+room_list = [room.show1(), room.show2()]
 def select_floor():
     num = input('방을 선택하세요')
     if num == '1':
-        print('죽음')
+        room.show1()
     elif num == '2':
-        room.show()
+        room.show2()
 
 select_floor()
--- 2400/수행평가/room.py
+++ 2400/수행평가/room.py
@@ -1,7 +1,13 @@
-def show():
-    num = input('도구를 선택하세요')
+def show1():
+    num = input('1도구를 선택하세요')
     if num == '1':
         print('1 선택')
     elif num == '2':
         print('2 선택')
 
+def show2():
+    num = input('2도구를 선택하세요')
+    if num == '1':
+        print('1 선택')
+    elif num == '2':
+        print('2 선택')
