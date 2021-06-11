42e580dac80e058d47e093b32a46f9a7f2cb9d01
ssep4u <ssep4u@gmail.com>
2021-06-11 16:23:47 +0900

    재령이꺼. if -> list

--- 2400/수행평가/floor.py
+++ 2400/수행평가/floor.py
@@ -1,10 +1,8 @@
 import room
-room_list = [room.show1(), room.show2()]
+room_list = [room.show1, room.show2]
 def select_floor():
     num = input('방을 선택하세요')
-    if num == '1':
-        room.show1()
-    elif num == '2':
-        room.show2()
+    num = int(num) - 1  #'1' -> 0, '2' -> 1
+    room_list[num]()
 
 select_floor()
