76afb324a797f28192aae6bba4df4d2880e827f4
ssep4u <ssep4u@gmail.com>
2021-08-27 09:42:43 +0900

    module

---
+++ 2300/module, package/my_module.py
@@ -0,0 +1,19 @@
+def 지원이():
+    print('택배뜯기')
+
+def 민주():
+    print('음악듣기')
+
+date = '2021.8.27'
+
+# print(__name__)
+if __name__ == '__main__':
+    지원이()
+    민주()
+    print(date)
+
+
+
+
+
+
