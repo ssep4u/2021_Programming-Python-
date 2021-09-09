ea5117a87750882bf1b0fbbf99aa8ad899379ee4
ssep4u <ssep4u@gmail.com>
2021-09-10 08:57:40 +0900

    랜덤자리배치하기

--- 2300/module, package/quiz.py
+++ 2300/module, package/quiz.py
@@ -16,11 +16,12 @@ print(round(score, -1))
 # 3. 로또 복권 자동 생성기를 만든다면?
 # (로또복권: 1~45 사이의 번호 중 랜덤으로 6개 뽑기)
 import random
+
 print(random.sample(range(1, 46), 6))
 
 # 4. 1~9 숫자로 중복되지 않은 숫자 세자리를 뽑는 방법은?(185:O, 212:X)
 list_r = random.sample(range(1, 9 + 1), 3)
-print("".join(str(n) for n in list_r))       #"273"
+print("".join(str(n) for n in list_r))  # "273"
 print("".join(map(str, list_r)))
 string = ''
 for n in list_r:
@@ -29,6 +30,7 @@ print(string)
 
 # 5. 내가 태어난 날로부터 며칠이 지났는지?
 import datetime
+
 birthday = datetime.datetime(2004, 1, 4)
 tbirthday = datetime.datetime(1980, 3, 31)
 now = datetime.datetime.now()
@@ -50,31 +52,25 @@ print(yj_birthday - now)
 
 # 8. 랜덤하게 번호로 자리를 배치하는 방법은?
 # 제적(전출, 자퇴) 인원이 있다면?
-#마지막 번호가 몇 번인지 묻자
+# 마지막 번호가 몇 번인지 묻자
 last_number = input("마지막 번호는?")
-#1~마지막번호까지 리스트 만들자
+# 1~마지막번호까지 리스트 만들자
 list_class = list(range(1, int(last_number) + 1))
 
-#무한반복
+# 무한반복
 while True:
-#  나간 친구 번호 묻자
+    #  나간 친구 번호 묻자
     exclude_number = input("뺄 번호는?(그냥 enter치면 끝내자)")
-#  그냥 enter면 반복 끝내자
+    #  그냥 enter면 반복 끝내자
     if exclude_number == '':
         break
-#  리스트에서 빼자
+    #  리스트에서 빼자
     list_class.remove(int(exclude_number))
-    print(list_class)
 
-#랜덤으로 섞자
-#출력하자
-
-
-
-
-
-
-
-
-
-
+# 랜덤으로 섞자
+random.shuffle(list_class)
+# 출력하자
+# print(list_class)
+print('자리\t학생번호')
+for index, n in enumerate(list_class):
+    print(f'{index + 1}\t{n}')
