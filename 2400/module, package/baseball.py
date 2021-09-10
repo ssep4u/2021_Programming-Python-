c1a8edec3731a3f827a996e65fea031e0350959c
ssep4u <ssep4u@gmail.com>
2021-09-10 15:27:23 +0900

    야구게임엔진 완성

---
+++ 2400/module, package/baseball.py
@@ -0,0 +1,14 @@
+answer = make_answer()
+print(answer)
+#무한반복
+while True:
+#  숫자 묻자
+    guess = input("뭘까?")
+#  strike, ball 판정하자
+    strike, ball = check(guess, answer)
+#  출력하자
+    print(f'{guess}\tstrike: {strike}, ball: {ball}')
+#  정답 == 숫자, 끝내자
+    if answer == guess:
+        print('정답입니다.')
+        break
