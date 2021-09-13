63579c62b09206cf37f9497738fc83799b519a15
ssep4u <ssep4u@gmail.com>
2021-09-13 13:40:03 +0900

    예외처리 연습

---
+++ 2400/except/except2.py
@@ -0,0 +1,5 @@
+print(0/10) #0
+# print(10/0) #ZeroDivisionError: division by zero
+print('name')
+# print(name)     #NameError: name 'name' is not defined
+print('age:'+18)    #TypeError: can only concatenate str (not "int") to str
