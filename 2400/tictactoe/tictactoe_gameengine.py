2ba29773c56793746b24f2f9ce2dd0df7d738f1f
SSep <ssep4u@gmail.com>
2021-11-05 14:51:22 +0900

    tictactoe game engine 완성

--- 2400/tictactoe/tictactoe_gameengine.py
+++ 2400/tictactoe/tictactoe_gameengine.py
@@ -29,55 +29,56 @@ class TictactoeGameEngine:
                     == self.board[self.position_to_index(row, 2)] \
                     == self.board[self.position_to_index(row, 3)] \
                     == self.turn:
-                return '-'
+                return self.turn
         # |줄
         for col in range(1, 3 + 1):
             if self.board[self.position_to_index(1, col)] \
                     == self.board[self.position_to_index(2, col)] \
                     == self.board[self.position_to_index(3, col)] \
                     == self.turn:
-                return '|'
+                return self.turn
         #/
         if self.board[self.position_to_index(1, 3)] \
             == self.board[self.position_to_index(2, 2)] \
             == self.board[self.position_to_index(3, 1)] \
             == self.turn:
-            return '/'
+            return self.turn
         #\
         if self.board[self.position_to_index(1, 1)] \
             == self.board[self.position_to_index(2, 2)] \
             == self.board[self.position_to_index(3, 3)] \
             == self.turn:
-            return '\\'
+            return self.turn
         # 클링이2김1박1 김지함
         # - 3줄 : 1,2,3 | 4,5,6 | 7,8,9 -> 0,1,2 | 3,4,5 | 6,7,8
-        for i in range(0, 7):
-            if i == 0 or i == 3 or i == 6:
-                if self.board[i] == self.board[i + 1] == self.board[i + 2]:
-                    print("-")
-        # | 3줄 1,4,7 | 2,5,8 | 3,6,9 -> 0,3,6 | 1,4,7 | 2,5,8
-        for i in range(0, 3):
-            if i == 0 or i == 1 or i == 2:
-                if self.board[i] == self.board[i + 3] == self.board[i + 6]:
-                    print("|")
+        # for i in range(0, 7):
+        #     if i == 0 or i == 3 or i == 6:
+        #         if self.board[i] == self.board[i + 1] == self.board[i + 2]:
+        #             print("-")
+        # # | 3줄 1,4,7 | 2,5,8 | 3,6,9 -> 0,3,6 | 1,4,7 | 2,5,8
+        # for i in range(0, 3):
+        #     if i == 0 or i == 1 or i == 2:
+        #         if self.board[i] == self.board[i + 3] == self.board[i + 6]:
+        #             print("|")
 
-        # 포피포피 고에스더
-        # /
-        if self.board[2] == 'X' and self.board[4] == 'X' and self.board[6] == 'X':
-            return print('X 승리')
-        if self.board[2] == 'O' and self.board[4] == 'O' and self.board[6] == 'O':
-            return print('O 승리')
-        # \
-        if self.board[0] == 'X' and self.board[4] == 'X' and self.board[8] == 'X':
-            return print('X 승리')
-        if self.board[0] == 'O' and self.board[4] == 'O' and self.board[8] == 'O':
-            return print('O 승리')
+        # # 포피포피 고에스더
+        # # /
+        # if self.board[2] == 'X' and self.board[4] == 'X' and self.board[6] == 'X':
+        #     return print('X 승리')
+        # if self.board[2] == 'O' and self.board[4] == 'O' and self.board[6] == 'O':
+        #     return print('O 승리')
+        # # \
+        # if self.board[0] == 'X' and self.board[4] == 'X' and self.board[8] == 'X':
+        #     return print('X 승리')
+        # if self.board[0] == 'O' and self.board[4] == 'O' and self.board[8] == 'O':
+        #     return print('O 승리')
 
         # 끝나는 경우 : 무승부(승자가 없는 상태로 놓을 자리가 없음), 승자 결정(승자가 있음)
-        if self.board[0] != '.' and self.board[1] != '.' and self.board[2] != '.' and \
-                self.board[3] != '.' and self.board[4] != '.' and self.board[5] != '.' and \
-                self.board[6] != '.' and self.board[7] != '.' and self.board[8] != '.':
-            return print('무승부')
+        # if self.board[0] != '.' and self.board[1] != '.' and self.board[2] != '.' and \
+        #         self.board[3] != '.' and self.board[4] != '.' and self.board[5] != '.' and \
+        #         self.board[6] != '.' and self.board[7] != '.' and self.board[8] != '.':
+        if not '.' in self.board:
+            return 'd'  #draw
 
     def change_turn(self):  # 와르르 노예린
         # if self.turn == 'X':
