81600d63c3aa608ef98c89d811f5ba4b35c636f0
SSep <ssep4u@gmail.com>
2021-11-12 10:02:55 +0900

    tictactoe GUI draw_board

--- 2300/tictactoe/tictactoe_GUI.py
+++ 2300/tictactoe/tictactoe_GUI.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 
 from tictactoe_game_engine import TictactoeGameEngine
 
@@ -12,35 +13,55 @@ class TictactoeGUI:
         self.CANVAS_SIZE = 300
         self.root = tkinter.Tk()
         self.root.title('틱택토')
-        self.root.geometry(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')   #'300x300'
+        self.root.geometry(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')  # '300x300'
         self.root.resizable(width=False, height=False)
 
         self.canvas = tkinter.Canvas(self.root, bg='white', width=self.CANVAS_SIZE, height=self.CANVAS_SIZE)
         self.canvas.pack()
 
-        self.images = {}        #{'X': PhotoImage객체, 'O': PhotoImage객체}
+        self.images = {}  # {'X': PhotoImage객체, 'O': PhotoImage객체}
         self.images['X'] = tkinter.PhotoImage(file='X.gif')
         self.images['O'] = tkinter.PhotoImage(file='O.gif')
 
-        self.canvas.bind('<Button-1>', self.click_handler)  #***
+        self.canvas.bind('<Button-1>', self.click_handler)  # ***
 
         self.root.mainloop()
 
-
     def click_handler(self, event):
-        #input event.x, event.y -> row, col
+        # input event.x, event.y -> row, col
         row, col = self.coordinate_to_position(event.x, event.y)
-        #set row, col
+        # set row, col
         self.game_engine.set(row, col)
-        #show board
-        self.game_engine.show_board()
-        #set winner
-        #승자가 있거나 무승부일 때, 게임오버, 결과 출력하자
-        #change turn
-
+        # show board
+        self.draw_board()
+        # set winner
+        winner = self.game_engine.set_winner()
+        # 승자가 있거나 무승부일 때, 게임오버, 결과 출력하자
+        if winner == 'X' or winner == 'O':
+            messagebox.showinfo('Game Over', f'{winner} 이김🎊')
+            self.root.quit()
+        elif winner == 'd':
+            messagebox.showinfo('Game Over', '무승부🤞')
+            self.root.quit()
+        # change turn
+        self.game_engine.change_turn()
 
     def draw_board(self):
-        pass
+        TILE_SIZE = self.CANVAS_SIZE // self.game_engine.SIZE  # 100
+        # clear
+        self.canvas.delete('all')
+
+        x = 0
+        y = 0
+        for i, v in enumerate(self.game_engine.board):
+            if v == '.':
+                pass
+            else:  # elif v == 'X' or v == 'O':
+                self.canvas.create_image(x, y, anchor='nw', image=self.images[v])
+            x += TILE_SIZE
+            if i % self.game_engine.SIZE == self.game_engine.SIZE - 1:
+                x = 0
+                y += TILE_SIZE
 
     def coordinate_to_position(self, x, y):
         # if 0 <= x < 100:
@@ -65,15 +86,3 @@ class TictactoeGUI:
 
 if __name__ == '__main__':
     ttt_GUI = TictactoeGUI()
-
-
-
-
-
-
-
-
-
-
-
-
